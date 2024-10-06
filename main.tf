b246a00f4642fcec7be8d64ecde8db0cbe22f162
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-06 08:35:05 -0400

    edit each.value for instance_type

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "tools" {
   source = "./module"
 
   tool_name= each.key
-  instance_type= each.key["instance_type"]
+  instance_type= each.value["instance_type"]
   zone_id = var.zone_id
 }
 #variable "tools" {
