aa99c5b2ff45a9ffac2fddf82b778c9225c51d24
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-07 14:01:33 -0400

    edit aws router ......

--- module/main.tf
+++ module/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
-  name    = var.tool_name
+  name    = "${var.component}-${var.env}"
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
