3ab0f5ae2bdc8f78d759ef4a86ea3d3bb7da785b
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-07 19:39:26 -0400

    edit

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.selected.id]
+  ami                   = data.aws_ami.ami.image_id
+  instance_type         = var.instance_type
+  vpc_security_group_ids= [data.aws_security_group.selected.id]
 
  tags = {
    Name= var.tool_name
@@ -12,6 +12,6 @@ resource "aws_route53_record" "record" {
   name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
-  records = [aws_instance.instance.public_ip]
-  ttl = 3
+  records = [aws_instance.instance.private_ip]
+  ttl = 30
 }
