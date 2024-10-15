abd2c1a210c1254dfcec9156cc80a33fa33e9838
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-15 11:40:30 -0400

    add d vault in variables.tf instance

--- variables.tf
+++ variables.tf
@@ -9,7 +9,10 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
-
+    vault = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
+    }
   }
 }
 
