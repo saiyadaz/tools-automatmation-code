62ef7d4e6fc87d5864a2ab3fa6504ea0c9233627
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-21 16:26:09 -0400

    removing vautlt

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "tools" {
       policy_resource_list = []
     }
 
-   # vault = {
-      #instance_type        = "t3.small"
-      #policy_resource_list = []
-    #}
+    vault = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
+    }
 
     variable "zone_id" {
     default = "Z0599937U1I5C34JZJE7"
