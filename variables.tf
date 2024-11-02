07b0d9c65b4da2dc0654972083555ba3efdaaccf
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-01 21:29:01 -0400

    edit

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type        = "t3.micro"
-      policy_resource_list = ["ec2:DescribeInstances"]
-    }
+   # prometheus = {
+      #instance_type        = "t3.micro"
+      #policy_resource_list = ["ec2:DescribeInstances"]
+    #}
 
-    jenkins = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
-    }
+    #jenkins = {
+    #  instance_type        = "t3.small"
+     # policy_resource_list = []
+    #}
 
     vault = {
       instance_type        = "t3.small"
