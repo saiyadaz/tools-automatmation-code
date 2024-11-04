e940147fceaf8476155d55d304708628e1f30c4b
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-04 10:15:59 -0500

    sonarqube

--- variables.tf
+++ variables.tf
@@ -19,6 +19,12 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = []
 
+
+    sonarqube = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
+
+      }
     } }
 }
 
