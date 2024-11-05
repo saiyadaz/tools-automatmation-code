6a58e24001cbe184e5d0b86b39580b16c238678e
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-04 19:57:52 -0500

    edit tools variables.tf

--- variables.tf
+++ variables.tf
@@ -19,20 +19,17 @@ variable "tools" {
       #instance_type        = "t3.small"
       #policy_resource_list = []
 
-    }
-    sonarqube = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
+   # }
+    #sonarqube = {
+     # instance_type        = "t3.small"
+      #policy_resource_list = []
 
       }
-
-  }
-
-
-  variable "zone_id" {
-    default = "Z0599937U1I5C34JZJE7"
+}
+variable "zone_id" {
+  default = "Z0599937U1I5C34JZJE7"
     }
-
+  
 
 
 
