6b60323958b36bdccfd68b5c066e9ce50c5e6943
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-08 09:39:57 -0500

    edt

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "tools" {
       #policy_resource_list = ["ec2:DescribeInstances"]
     #}
 
-    jenkins = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
-    }
+    #jenkins = {
+    # instance_type        = "t3.small"
+    #policy_resource_list = []
+    # }
 
     vault = {
       instance_type        = "t3.small"
