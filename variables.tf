cdfcab767d84aaa7d729c3a4cf79200e61948598
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-24 13:21:21 -0500

    edit shared lib var jenkins

--- variables.tf
+++ variables.tf
@@ -20,14 +20,14 @@ variable "tools" {
       policy_resource_list = ["ecr:*", "eks:*", "ec2:*"]
 
     }
-    sonarqube = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
+    #sonarqube = {
+     # instance_type        = "t3.small"
+      #policy_resource_list = []
 
       }
 }
 
-}
+#}
 variable "zone_id" {
   default = "Z0599937U1I5C34JZJE7"
     }
